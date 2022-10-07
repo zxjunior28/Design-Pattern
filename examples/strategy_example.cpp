ac2577d3613b122a2cde96557cce0ead4cec1a9a
KatelynBai <katelynbai@moldex3d.com>
2022-10-07 18:33:48 +0800

    plz solve the incomplete type plz

--- examples/strategy_example.cpp
+++ examples/strategy_example.cpp
@@ -2,8 +2,14 @@
 #include <strategy/strategy.hpp>
 
 int main(int argc, char **argv) {
+    architecture::strategy::Waterball waterball;
+    architecture::strategy::Colliding colliding;
+    // architecture::strategy::
+    architecture::strategy::Hero hero1("Kavy", waterball);
+    architecture::strategy::Hero hero2("Jacky", colliding);
 
-    
+
+    hero1.attack(hero2);
 
     return 0;
 }
--- src/strategy/strategy.hpp
+++ src/strategy/strategy.hpp
@@ -10,19 +10,49 @@ class Hero;
 
 class ISkill {
  public:
-    ISkill();
-    ~ISkill();
+    ISkill() = default;
+    ~ISkill() = default;
     virtual int attack(Hero attaking_hero, Hero attaked_hero);
+};
+
+class  Waterball : public ISkill {
+ public:
+    Waterball() = default;
+    ~Waterball() = default;
+    int attack(Hero attaking_hero, Hero target) override {
+        attaking_hero.lostMp(5);
+        int injury = attaking_hero.getWisdom()*2;
+        target.lostHp(injury);
+        retrun injury;
+    }
+};
+
+class Colliding : public ISkill{
+ public:
+    Colliding() = default;
+    ~Colliding() = default;
+    int attack(Hero attaking_hero, Hero target) override {
+        int injury = attaking_hero.getStrength() - target.getDefense();
+        target.lostHp(injury);
+        return injury;
+    }
 };
 
 class Hero {
  public:
+    Hero() = default;
     Hero(string_type name, ISkill skill) : name_(name), skill_(skill) {}
     ~Hero() = default;
 
     void attack(Hero target);
     string_type getName() {return name_;}
     int getHp() {return hp_ <= 0 ? 0 : hp_;}
+    int getWisdom() {return wisdom_;}
+    int getStrength() {return strength_;}
+    int getDefense() {return defense_;}
+    void lostHp(int hp) {hp_ -= hp;}
+    void lostMp(int mp) {mp_ -= mp;}
+
  private:
     string_type name_;
     int hp_{500};
