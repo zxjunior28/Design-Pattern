be8dcfb24320cbd03754d498ddd5b79260f5a21d
KatelynBai <katelynbai@moldex3d.com>
2023-01-30 09:04:33 +0800

    added prototype

---
+++ examples/prototype_example.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Katelyn Bai
+#include <prototype/prototype.hpp>
+
+int main(int argc, char **argv) {
+    std::cout << "hi~ prototype!" << std::endl;
+    return 0;
+}
---
+++ src/prototype/prototype.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Katelyn Bai
+#ifndef SRC_PROTOTYPE_PROTOTYPE_HPP_
+#define SRC_PROTOTYPE_PROTOTYPE_HPP_
+
+#include <architecture/definitions.hpp>
+
+namespace architecture::prototype {
+
+
+
+}  /// namespace architecture::prototype
+#endif  // SRC_PROTOTYPE_PROTOTYPE_HPP_
